8693d232e4645f336f4b09b648b021eba558514e
Butter_potato <floria59@naver.com>
2023-03-03 22:30:24 +0900

    230303 코딩테스트연습

---
+++ programmers/codingtest/lv0/test1.java
@@ -0,0 +1,16 @@
+package programmers;
+
+
+// 배열 두배 만들기
+public class Solution {
+    public int[] solution(int[] numbers) {
+        
+        int[] answer = new int[numbers.length];
+        
+        for(int i = 0; i < answer.length; i++) {
+            answer[i] = numbers[i] * 2;
+        }
+        
+        return answer;
+    }
+}
